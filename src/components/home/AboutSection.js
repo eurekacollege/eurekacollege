2ed25b36d82a65347cae702f53865389cbe72ffe
eurekacollege <eurekacollegecanada@gmail.com>
2023-05-04 00:00:42 -0700

    fixed video

--- src/components/home/AboutSection.js
+++ src/components/home/AboutSection.js
@@ -36,7 +36,7 @@ export default function AboutSection() {
         <Link href="https://fnrc.ca/" target="_blank" rel="noopener noreferrer" className={styles.link2}>Learn More</Link>
       </div>
       <div className={styles.videoContainer}>
-        <video src="/images/blinkingeyes.mp4" width={500} height={500} alt="image" autoPlay loop/>
+        <video src="/images/blinkingeyes.mp4" width={1500} height={1500} alt="image" autoPlay loop/>
       </div>
     </div>
   );
