b3b671ed39d3a2d74c74c449f7a2231f1a993fd1
eurekacollege <eurekacollegecanada@gmail.com>
2023-05-03 23:56:53 -0700

    fixed favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <meta property="og:image" content="https://www.yourwebsite.com/images/new-logo.png" />
-      <link rel="icon" href="/images/favlogo.png" type="image/x-icon" />
+      {/* <link rel="icon" href="/images/favlogo.png" type="image/x-icon" /> */}
+      <link rel="shortcut icon" href="/images/favlogo.png" type="image/png" />
       </Head>
       <body>
         <Main />
