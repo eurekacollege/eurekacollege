6adb05d07caa556de17829d1704a63b755027385
eurekacollege <eurekacollegecanada@gmail.com>
2023-05-21 01:35:24 -0700

    removed headliner paragraph for courses

--- src/components/courses/CourseHeadliner.js
+++ src/components/courses/CourseHeadliner.js
@@ -6,12 +6,12 @@ export default function CourseHeadliner() {
     <div className={styles.mainHeadliner}>
       <div className={styles.wordContainer}>
         <p className={styles.word1}>OUR COURSES</p>
-        <p className={styles.word2}>
+        {/* <p className={styles.word2}>
           Welcome to Eureka College of Canada, where we offer a wide range of programs to
           help you achieve your goals. Whether you're looking to start a new
           career, enhance your skills, or simply learn something new, we've got
           you covered.
-        </p>
+        </p> */}
       </div>
     </div>
   );
